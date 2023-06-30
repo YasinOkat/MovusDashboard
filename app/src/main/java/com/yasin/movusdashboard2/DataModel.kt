f1d294222d5a03755df32a3b747f7c153fb90c7c
YasinOkat <yasinokat@gmail.com>
2023-06-30 07:48:39 +0300

    Added

---
+++ app/src/main/java/com/yasin/movusdashboard2/DataModel.kt
@@ -0,0 +1,28 @@
+package com.yasin.movusdashboard2
+
+data class MyDataModel(
+    val days: String? = null,
+    val rampa_yanasma: String? = null,
+    val rampa_cikis: String? = null,
+    val toplama_baslama: String? = null,
+    val toplama_bitis: String? = null,
+    val yukleme_baslama: String? = null,
+    val yukleme_bitis: String? = null,
+    val dusum_dk: String? = null,
+    val ilave_dk: String? = null,
+    val dusum_adet: String? = null,
+    val ilave_adet: String? = null,
+    val block_dk: String? = null,
+    val bloke_adet: String? = null,
+    val diger_dk: String? = null,
+    val diger_adet: String? = null,
+    val vardiya_amiri: String? = null,
+    val rampa_sorumlusu: String? = null,
+    val yuklemeci: String? = null,
+    val yuklemeci2: String? = null,
+    val arac_plaka: String? = null,
+    val dorse_plaka: String? = null,
+    val urun_sayisi: String? = null,
+    val forklift_operatoru: String? = null
+)
+
--- app/src/main/java/com/yasin/movusdashboard2/MainActivity.kt
+++ app/src/main/java/com/yasin/movusdashboard2/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.yasin.movusdashboard2
 
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import com.yasin.movusdashboard2.databinding.ActivityMainBinding
-import okhttp3.HttpUrl
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.*
 import org.json.JSONArray
 import org.json.JSONObject
-import okhttp3.*
 import java.io.IOException
-
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,7 +33,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-
         handler.post(apiUpdateRunnable)
     }
 
@@ -48,15 +45,14 @@ class MainActivity : AppCompatActivity() {
         val client = OkHttpClient()
 
         val request = Request.Builder()
-            .url("http://192.168.1.193:5000/getLatestDaysNo")
+            .url("http://192.168.1.193:5000/getInfo")
             .get()
             .build()
-
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 runOnUiThread {
-                    Toast.makeText(this@MainActivity, "Failed to retrieve days numbers", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this@MainActivity, "Failed to retrieve data", Toast.LENGTH_LONG).show()
                 }
             }
 
@@ -64,21 +60,75 @@ class MainActivity : AppCompatActivity() {
                 if (response.isSuccessful) {
                     val responseData = response.body?.string()
 
-                    val jsonArray = JSONArray(responseData)
-                    val jsonObject = jsonArray.getJSONObject(0)
-                    val days = jsonObject.getString("days")
-
-                    Log.d("MainActivity", "Fetched days: $days")
+                    val gson = Gson()
+                    val myData = gson.fromJson(responseData, MyDataModel::class.java)
 
                     runOnUiThread {
-                        binding?.tvSistemNo?.text = days
+                        val dateFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH)
+                        val date = if (!myData.rampa_yanasma.isNullOrEmpty()) {
+                            dateFormat.parse(myData.rampa_yanasma)
+                        } else {
+                            null
+                        }
+
+                        val rampa_yanasma_saat = if (date != null) {
+                            val calendar = Calendar.getInstance()
+                            calendar.time = date
+                            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+                            val minute = calendar.get(Calendar.MINUTE)
+                            "$hour:$minute"
+                        } else {
+                            ""
+
+                        }
+
+                        val dusumAdet = myData.dusum_adet?.toIntOrNull() ?: 0
+                        val ilaveAdet = myData.ilave_adet?.toIntOrNull() ?: 0
+                        val blokeAdet = myData.bloke_adet?.toIntOrNull() ?: 0
+                        val digerAdet = myData.diger_adet?.toIntOrNull() ?: 0
+
+                        val dusumDk = extractNumberFromString(myData.dusum_dk) ?: 0
+                        val ilaveDk = extractNumberFromString(myData.ilave_dk) ?: 0
+                        val blokeDk = extractNumberFromString(myData.block_dk) ?: 0
+                        val digerDk = extractNumberFromString(myData.diger_dk) ?: 0
+
+                        binding?.tvSistemNo?.text = myData.days
+                        binding?.tvYuklemeBaslama?.text = rampa_yanasma_saat
+                        binding?.tvYuklemeBitis?.text = myData.rampa_cikis
+                        binding?.tvDusumDk?.text = dusumDk.toString()
+                        binding?.tvDusumAdet?.text = dusumAdet.toString()
+                        binding?.tvIlaveAdet?.text = ilaveAdet.toString()
+                        binding?.tvBlokeDk?.text = blokeDk.toString()
+                        binding?.tvBlokeAdet?.text = blokeAdet.toString()
+                        binding?.tvDigerAdet?.text = digerAdet.toString()
+                        binding?.tvIlaveDk?.text = ilaveDk.toString()
+                        binding?.tvDigerDk?.text = digerDk.toString()
+                        binding?.tvVardiyaAmiri?.text = myData.vardiya_amiri?.uppercase()
+                        binding?.tvRampaGorevlisi?.text = myData.rampa_sorumlusu?.uppercase()
+                        binding?.tvYuklemeci1?.text = myData.yuklemeci?.uppercase()
+                        binding?.tvYuklemeci2?.text = myData.yuklemeci2?.uppercase()
+                        binding?.tvForkliftOper?.text = myData.forklift_operatoru?.uppercase()
+
+                        val totalAdet = dusumAdet + ilaveAdet + blokeAdet + digerAdet
+                        binding?.tvToplamAdet?.text = totalAdet.toString()
+                        val totalDk = dusumDk + ilaveDk + blokeDk + digerDk
+                        binding?.tvToplamDk?.text = totalDk.toString()
                     }
                 } else {
                     runOnUiThread {
-                        Toast.makeText(this@MainActivity, "Failed to retrieve days numbers", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this@MainActivity, "Failed to retrieve data", Toast.LENGTH_LONG).show()
                     }
                 }
             }
         })
     }
+
+
+    private fun extractNumberFromString(input: String?): Int? {
+        val numberRegex = "\\d+".toRegex()
+        val matchResult = numberRegex.find(input ?: "")
+        return matchResult?.value?.toIntOrNull()
+    }
+
+
 }
